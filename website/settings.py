4beb301c6dff419447db939e7ca9c5ac27ab2d97
bearz314 <wongchoonjie@gmail.com>
2022-07-27 20:42:36 +1000

    Revert url

--- website/settings.py
+++ website/settings.py
@@ -6,6 +6,6 @@ SECRET_KEY = 'secret-key'
 # https://openid.net/specs/openid-connect-core-1_0.html#IDToken
 OAUTH2_JWT_ENABLED = True                # Not implemented
 OAUTH2_JWT_KEY = 'secret-key'            # REQUIRED Should be strong
-OAUTH2_JWT_ISS = 'https://authlib3.org'  # REQUIRED Should be https, no query/fragment
+OAUTH2_JWT_ISS = 'https://authlib.org'   # REQUIRED Should be https, no query/fragment
 OAUTH2_JWT_ALG = 'HS256'                 # REQUIRED unless no token ID
 OAUTH2_JWT_EXP = 3600                    # REQUIRED
